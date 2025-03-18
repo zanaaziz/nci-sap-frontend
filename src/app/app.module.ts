cea650407c9d6de953419444993d9dab5220e895
Zana Aziz <mail@zanaaziz.com>
2025-03-18 14:00:41 +0000

    testing bootstrap and material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatButtonModule} from '@angular/material/button';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
