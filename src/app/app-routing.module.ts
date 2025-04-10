2ed14709be0b50aee5f7ab6535cdae3fb73dd427
Zana Aziz <mail@zanaaziz.com>
2025-04-10 15:07:51 +0100

    added redirect based on auth state

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
+	{ path: '', redirectTo: '/auth', pathMatch: 'full' },
 	{
 		path: 'auth',
+		canActivate: [AuthGuard],
 		loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
 	},
 	{
 		path: 'editor',
+		canActivate: [AuthGuard],
 		loadChildren: () => import('./editor/editor.module').then((m) => m.EditorModule),
 	},
 ];
